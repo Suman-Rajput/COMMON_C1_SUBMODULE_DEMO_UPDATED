1471b0fe1387a526a6dd1a496689210282b56ecb
Suman-Rajput <sumanrajputmit@gmail.com>
2025-02-03 01:31:12 +0530

    added changes in child class in eclipse nd seeing it will reflect or not in repo of parent >>submodule

--- src/main/java/CommonClass.java
+++ src/main/java/CommonClass.java
@@ -1,8 +1,15 @@
 
 public class CommonClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("DISPLAY GITHUB COMMON CLASS SUBMODULE");
+		
+		System.out.println("Want to see this changes will reflect in submodule or not which is present in parent parent repo");
+		
+		System.out.println("RESULT NO CHANGES YOU CAN SEE IN SUBMODULE ");
+		
+		System.out.println("NO");
 
 	}
 
