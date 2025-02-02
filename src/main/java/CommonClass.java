5b53978059a6404dffcb224453c27615ec8ed19a
Suman-Rajput <sumanrajputmit@gmail.com>
2025-02-02 20:52:20 +0530

    added file inside parent

--- src/main/java/CommonClass.java
+++ src/main/java/CommonClass.java
@@ -1,9 +1,13 @@
 
 public class CommonClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		System.out.println("DISPLAY GITHUB COMMON CLASS SUBMODULE");
 
+		System.out.println("ADDED SUBMODULE TASK 1");
+		
+		
 	}
 
 }
