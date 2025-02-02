081a6eb45d3cf93f7c81092d84cfceeecb057675
Suman-Rajput <sumanrajputmit@gmail.com>
2025-02-03 01:41:21 +0530

    changes in submodule class

--- src/main/java/CommonClass.java
+++ src/main/java/CommonClass.java
@@ -1,11 +1,7 @@
 
 public class CommonClass {
 
-<<<<<<< HEAD
 	public static void main(String[] args) 
-=======
-	public static void main(String[] args)
->>>>>>> 5b53978059a6404dffcb224453c27615ec8ed19a
 	{
 		System.out.println("DISPLAY GITHUB COMMON CLASS SUBMODULE");
 		
